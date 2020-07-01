13e377782a3ea6807ee4df302ed7a59cd0b4011e
JulieAllix <allixjulie@gmail.com>
2020-07-01 13:14:39 +0200

    Create Colors.js file

---
+++ constants/Colors.js
@@ -0,0 +1,13 @@
+export default {
+    /*
+    primaryColor: '#39A0ED', // Carolina Blue
+    primaryColor: '#0A0908', // Rich Black FOGRA 39
+    primaryColor: '#0E1428', // Oxford Blue
+    */
+    primaryColor: '#313638', // Onyx
+    /*
+    accentColor: '#3B1F2B', // Dark Purple
+    accentColor: '#F9A620', // Bright Yellow Crayola
+    */
+    accentColor: '#F06543', // Orange Soda
+};
