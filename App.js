d31c62fb0f61e1156801b7135a7b53affe45cd15
JulieAllix <allixjulie@gmail.com>
2020-07-01 13:23:48 +0200

    Create screen files

--- App.js
+++ App.js
@@ -35,7 +35,7 @@ export default function App() {
     <SafeAreaView style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       {/*<StatusBar style="auto" />*/}
-      </SafeAreaView>
+    </SafeAreaView>
   );
 }
 
