386e205fd0b43667b9d62c6188bb5c9357bf4910
JulieAllix <allixjulie@gmail.com>
2020-07-01 13:20:12 +0200

    Set up fonts

--- App.js
+++ App.js
@@ -1,12 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+//import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import * as Font from 'expo-font';
+import { AppLoading } from 'expo';
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'notable': require('./assets/fonts/Notable-Regular.ttf'),
+    'noto': require('./assets/fonts/NotoSans-Regular.ttf'),
+    'noto-b': require('./assets/fonts/NotoSans-Bold.ttf')
+  });
+}
+
 export default function App() {
+  const [dataLoaded, setDataLoaded] = useState(false);
+ 
+  if (!dataLoaded) {
+    return (
+      <AppLoading 
+        startAsync={fetchFonts} 
+        onFinish={() => setDataLoaded(true)} 
+        onError={() => console.log(err)}
+      />
+    );
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/*<StatusBar style="auto" />*/}
     </View>
   );
 }
