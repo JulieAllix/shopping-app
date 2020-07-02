a4caa25cbdee2de55c9d411698e1e6f55797580e
JulieAllix <allixjulie@gmail.com>
2020-07-02 12:17:17 +0200

    Changed the products data

--- data/dummy-data.js
+++ data/dummy-data.js
@@ -4,50 +4,50 @@ const PRODUCTS = [
   new Product(
     'p1',
     'u1',
-    'Red Shirt',
-    'https://cdn.pixabay.com/photo/2016/10/02/22/17/red-t-shirt-1710578_1280.jpg',
-    'A red t-shirt, perfect for days with non-red weather.',
-    29.99
+    'The Cacti Sisters',
+    'https://cdn.pixabay.com/photo/2017/03/04/22/24/cactus-2117102_960_720.jpg',
+    'A set of three adorable cacti to add some spice to your home decoration ! Do not water them too often, remember these babies are not that thirsty.',
+    9.99
   ),
   new Product(
     'p2',
     'u1',
-    'Blue Carpet',
-    'https://images.pexels.com/photos/6292/blue-pattern-texture-macro.jpg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    'Fits your red shirt perfectly. To stand on. Not to wear it.',
-    99.99
+    'Alan the Aloe',
+    'https://cdn.pixabay.com/photo/2015/12/08/00/35/plant-1081856_960_720.jpg',
+    'Alan is a cute Aloe Vera that will be happy to fill your living room with beauty. He is part of the succulents family, and therefore doesn\'t need to be watered that much.',
+    5.99
   ),
   new Product(
     'p3',
     'u2',
-    'Coffee Mug',
-    'https://images.pexels.com/photos/160834/coffee-cup-and-saucer-black-coffee-loose-coffee-beans-160834.jpeg?cs=srgb&dl=bean-beans-black-coffee-160834.jpg&fm=jpg',
-    'Can also be used for tea!',
-    8.99
+    'Basile the Bonzaï',
+    'https://cdn.pixabay.com/photo/2014/04/05/11/39/bonsai-316573_960_720.jpg',
+    'Basile looks like a very tiny tree, you\'ll love him ! It\'s quite difficult to take care of Bonzaïs, we recommend it for people who are already experimented with plants.',
+    19.99
   ),
   new Product(
     'p4',
     'u3',
-    'The Book - Limited Edition',
-    'https://images.pexels.com/photos/46274/pexels-photo-46274.jpeg?cs=srgb&dl=blur-blurred-book-pages-46274.jpg&fm=jpg',
-    "What the content is? Why would that matter? It's a limited edition!",
-    15.99
+    'Hervé the nice plant',
+    'https://cdn.pixabay.com/photo/2018/09/20/06/51/thank-you-3690116_960_720.jpg',
+    "Hervé is such a nice plant ! He writes thank you notes every time you water it. Aaaaaaaw !",
+    10.99
   ),
   new Product(
     'p5',
     'u3',
-    'PowerBook',
-    'https://get.pxhere.com/photo/laptop-computer-macbook-mac-screen-water-board-keyboard-technology-air-mouse-photo-airport-aircraft-tablet-aviation-office-black-monitor-keys-graphic-hardware-image-pc-exhibition-multimedia-calculator-vector-water-cooling-floppy-disk-phased-out-desktop-computer-netbook-personal-computer-computer-monitor-electronic-device-computer-hardware-display-device-448748.jpg',
-    'Awesome hardware, crappy keyboard and a hefty price. Buy now before a new one is released!',
-    2299.99
+    'The Bogdanov brothers',
+    'https://cdn.pixabay.com/photo/2018/05/02/16/58/fat-plants-3369055_960_720.jpg',
+    'The Bogdanov brothers are two succulents that won\'t teach you anything about space, but will make your home look nicer for sure !',
+    15.99
   ),
   new Product(
     'p6',
     'u1',
-    'Pen & Paper',
-    'https://cdn.pixabay.com/photo/2015/10/03/02/14/pen-969298_1280.jpg',
-    "Can be used for role-playing (not the kind of role-playing you're thinking about...).",
-    5.49
+    'Bono the Bonzaï',
+    'https://cdn.pixabay.com/photo/2018/02/18/14/38/leaf-3162421_960_720.jpg',
+    "Bono is a very surprising Bonzaï : he likes writing songs, therefore he is sold with this booknote (that you should always keep next to him !)",
+    24.49
   )
 ];
 
