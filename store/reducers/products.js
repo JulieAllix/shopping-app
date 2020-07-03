b6ec59e849bcb8f7a0260ff2a30409f2281b2501
JulieAllix <allixjulie@gmail.com>
2020-07-03 11:40:05 +0200

    Create ADD_TO_CART action

--- store/reducers/products.js
+++ store/reducers/products.js
@@ -1,12 +1,19 @@
 import PRODUCTS from '../../data/dummy-data';
+import { ADD_TO_CART } from '../actions/products';
 
 const initialState = {
     availableProducts: PRODUCTS,
+    productsInCart: [],
 };
 
 const productsReducer = (state = initialState, action) => {
-
-    return state;
+    switch (action.type) {
+        case ADD_TO_CART:
+            const product = state.availableProducts.find(product => product.id === action.productId);
+            return { ...state, productsInCart: state.productsInCart.concat(product) };
+        default:
+            return state;
+    }
 }
 
 export default productsReducer;
