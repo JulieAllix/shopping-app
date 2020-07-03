c4608f5a8bf483a19c16b4098951b5ffa3d4ddd8
JulieAllix <allixjulie@gmail.com>
2020-07-03 15:41:14 +0200

    Qties get reduced on click on trash

--- store/reducers/products.js
+++ store/reducers/products.js
@@ -9,22 +9,23 @@ const initialState = {
 
 const productsReducer = (state = initialState, action) => {
     const existingIndex = state.productsInCart.findIndex(product => product.id === action.productId);
+    
 
     switch (action.type) {
         case ADD_TO_CART:
+
             const product = state.availableProducts.find(product => product.id === action.productId);
             let newQtiesInCart = [];
-            //console.log(existingIndex);
+
             if (existingIndex >= 0) {
                 const product = state.qtiesInCart.find(product => product.id === action.productId);
                 const formerQty = product.qty;
-                console.log('formerQty');
-                console.log(formerQty);
+
                 newQtiesInCart = {
                     id: action.productId,
                     qty: formerQty + 1,
                 };
-                    console.log(state.qtiesInCart);
+
                 const updatedQties = [...state.qtiesInCart];
                 updatedQties.splice(existingIndex, 1, newQtiesInCart);
                 
@@ -47,10 +48,31 @@ const productsReducer = (state = initialState, action) => {
             }
 
         case REMOVE_FROM_CART:
-            const updatedCartItems = [...state.productsInCart];
-            
-            updatedCartItems.splice(existingIndex, 1);
-            return { ...state, productsInCart: updatedCartItems };
+            const removedProduct = state.qtiesInCart.find(product => product.id === action.productId);
+            const formerQty = removedProduct.qty;
+
+            if (formerQty === 1) {
+
+                const updatedCartItems = [...state.productsInCart];
+                updatedCartItems.splice(existingIndex, 1);
+    
+                return { ...state, productsInCart: updatedCartItems };
+
+            } else {
+
+                newQtiesInCart = {
+                    id: action.productId,
+                    qty: formerQty - 1,
+                };
+
+                const updatedQties = [...state.qtiesInCart];
+                updatedQties.splice(existingIndex, 1, newQtiesInCart);
+                
+                return { 
+                    ...state, 
+                    qtiesInCart: updatedQties
+                };
+            }
 
         default:
             return state;
