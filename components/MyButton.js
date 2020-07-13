2ee6ad575b1a99734e805e50a8dccee5fd51ab05
JulieAllix <allixjulie@gmail.com>
2020-07-13 15:25:30 +0200

    MyButton.js optimized

--- components/MyButton.js
+++ components/MyButton.js
@@ -1,10 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import { 
-    Dimensions, 
+import React from 'react';
+import {  
     Text, 
     TouchableOpacity, 
     StyleSheet,
-    Platform
 } from 'react-native';
 import { useSelector } from 'react-redux';
 
