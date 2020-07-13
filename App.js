55b09ca559fd2701ccd220ee5deabd034aa2edbe
JulieAllix <allixjulie@gmail.com>
2020-07-13 14:48:49 +0200

    App.js optimized

--- App.js
+++ App.js
@@ -1,22 +1,15 @@
-//import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { StyleSheet } from 'react-native';
 import { createStore, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
 
 import * as Font from 'expo-font';
 import { AppLoading } from 'expo';
 
-//import ShopNavigator from './navigation/ShopNavigator';
 import AppContainer from './components/AppContainer';
 
 import productsReducer from './store/reducers/products';
 import ordersReducer from './store/reducers/orders';
 import screenReducer from './store/reducers/screen';
-
-//import setScreenWidth from './store/actions/screen';
-
 
 const rootReducer = combineReducers({
   products: productsReducer,
@@ -28,7 +21,6 @@ const store = createStore(rootReducer);
 
 const fetchFonts = () => {
   return Font.loadAsync({
-    'notable': require('./assets/fonts/Notable-Regular.ttf'),
     'noto': require('./assets/fonts/NotoSans-Regular.ttf'),
     'noto-b': require('./assets/fonts/NotoSans-Bold.ttf'),
     'inconsolata': require('./assets/fonts/Inconsolata-Bold.ttf'),
@@ -54,12 +46,3 @@ export default function App() {
     </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
